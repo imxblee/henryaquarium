2fc6b3bbd08676e6df438ed0e8842dc5d2c57d84
imxblee <transcendencez@gmail.com>
2016-03-31 04:39:18 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Henryaquarium::Application.config.secret_key_base = '13f8d19276573421a27285da1cef2ac9f72c16865ddfd5909117f8998d3c415f6b3e64f83440b142016d2b135cb04607cd82efa324b3ea5ebd4e428a48fdb5b1'
