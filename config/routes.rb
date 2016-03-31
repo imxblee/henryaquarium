749e2543a4f4613b4457c2eb3123f169c805dc29
imxblee <transcendencez@gmail.com>
2016-03-31 05:05:34 +0000

    set up fishes controller and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Henryaquarium::Application.routes.draw do
+  root 'fishes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
